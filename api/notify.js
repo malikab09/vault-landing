2a5d78a18402e296f48bdd4c8f4e01407f0dbd2b
Malika Begum <malikab09@gmail.com>
2026-04-28 11:24:43 -0400

    Use RESEND_API_KEY env var instead of hardcoded key

--- api/notify.js
+++ api/notify.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   const response = await fetch('https://api.resend.com/emails', {
     method: 'POST',
     headers: {
-      'Authorization': 'Bearer re_JiUo9ZhL_2ceisGr22eGFaegfJS75jDU5',
+      'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
